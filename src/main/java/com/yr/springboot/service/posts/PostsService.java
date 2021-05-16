4de38efb3c4e9fa4987e39342e5aa22a5b5d7b10
yrin2 <yrin217@gmail.com>
2021-05-16 23:24:18 +0900

    4.4 게시글 작성까지 완료 indexControllerTest 에러 확인..

--- src/main/java/com/yr/springboot/service/posts/PostsService.java
+++ src/main/java/com/yr/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.yr.springboot.service.posts;
 
 import com.yr.springboot.domain.posts.Posts;
 import com.yr.springboot.domain.posts.PostsRepository;
+import com.yr.springboot.web.dto.PostsListResponseDto;
 import com.yr.springboot.web.dto.PostsResponseDto;
 import com.yr.springboot.web.dto.PostsSaveRequestDto;
 import com.yr.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 
